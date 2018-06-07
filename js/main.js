b816787f0ffc81ebd520453a7d9cd96a17c5b989
Philippe MEYER <p.meyer@mappingcontrol.com>
2018-06-07 09:16:44 +0200

    Starting over with basic trigo an camera only rotating not moving

--- js/main.js
+++ js/main.js
@@ -9,24 +9,38 @@ window.onload = function() {
 	var things = [];
 	var needUpdate = true;
 	
-	var fov = width /2;
+	var fov = width;
 	var w2 = width/2;
 	var h2 = height/2;
 	
 	var k90degres = toradians(90);
 	var k360degres = toradians(360);
+	
 	var camera = new Camera(0.1,20);
+	context.translate(width / 2, height / 2);
+// primitive,size,distance,altitude,angleToOrigine,rotation,name
+	var aRotation = {x:0.2,y:0.1,z:0};
+	var cubePrime = new Cube();
+	var nrOfCubes = 12;
+	var angleDiff = toradians(360/nrOfCubes);
+	var distance,size,altitude,angleToOrigine,name;
+	
+	for(var i = 0;i < nrOfCubes ;i++){
+		name = String.fromCharCode(65+i);
+		size = 20 + i*2;
+		altitude = 0;
+		distance = w2 - i*20;
+		angleToOrigine = angleDiff * i;
+		things.push(new Shape(cubePrime,size,distance,altitude,angleToOrigine,aRotation,name));
 
-	things.push(new Shape(new Cube(),80,{x:40,y:20,z:-300},{x:0.2,y:0.1,z:0},"one")),
-	//things.push(new Shape(new Cube(),40,{x:-150,y:20,z:100},{x:0.2,y:0,z:0},"two"));
+	}
 
 	update();
 
 function update() {
 	if(needUpdate){
 		
-		context.clearRect(0, 0 ,width, height);
-
+		context.clearRect(-w2 , -h2, width, height);
 		things.forEach(function(thing){
 			thing.draw();
 		});
@@ -37,21 +51,21 @@ function update() {
 }
 
 function Camera(rotStep,walkStep) {
-	this.rotation = {x:0,y:toradians(90),z:0};
+	this.rotation = 0; 
 	this.position = {x:0,y:0,z:0};
 	this.walkStep = walkStep;
 	this.rotStep = rotStep;
 	this.turn = function(amount){ // -1 or +1
 		//this.rotation.x += rotAngle.x;
-		this.rotation.y -= this.rotStep*amount;
-		if(this.rotation.y <0) this.rotation.y  += k360degres;
-		if(this.rotation.y >k360degres ) this.rotation.y  =0;
-		console.log("Camera : " + Math.floor(todegrees(this.rotation.y)));
+		this.rotation -= this.rotStep*amount;
+		if(this.rotation<0) this.rotation  += k360degres;
+		if(this.rotation >k360degres ) this.rotation  =0;
+		console.log("Camera : " + Math.floor(todegrees(this.rotation)));
 	}
 	this.walk = function(amount){// -1 or +1
 		// Calculate new position considering the amount, the position and the direction
-		var dirx = Math.sin(this.rotation.y);
-		var dirz = Math.cos(this.rotation.y);
+		var dirx = Math.sin(this.rotation);
+		var dirz = Math.cos(this.rotation);
 		this.position.x = Math.floor(this.position.x - (dirx * amount * this.walkStep)); 
 		this.position.z = Math.floor(this.position.z - (dirz * amount * this.walkStep));
 	}
@@ -112,12 +126,16 @@ return ax*bx+ay*by+az*bz;
 function hypo(x,y,z){
 return Math.sqrt(x*x+y*y+z*z);
 }
+// primitive,size,distance,altitude,angleToOrigine,rotation,name
 
-function Shape(geometry,shapeSize,shapePosition,shapeRotation,name){
+function Shape(geometry,size,distance,altitude,angleToOrigine,rotation,name){
 	
-	this.shapeSize = shapeSize;
-	this.shapePosition = shapePosition;
-	this.rotation = shapeRotation;
+	this.size = size;
+	this.distance = distance;
+	this.altitude = altitude;
+	this.angleToOrigine = angleToOrigine; // something to do with the cam rotation its only an integer
+	this.position = {"x":0,"y":altitude,"z":distance};	//initial position with camera at 0 degree
+	this.rotation = rotation;// nothing to do with the cam rotation
 	this.geometry = {};
     this.name = name;
 	
@@ -141,31 +159,30 @@ function Shape(geometry,shapeSize,shapePosition,shapeRotation,name){
 	
 	this.draw=function(){
 		
-		var centerOfItem = {"x":this.shapePosition.x,"y":this.shapePosition.y,"z":this.shapePosition.z}; // For (my) ease of comprehension !
-		//var dot=scalarProduct(this.shapePosition,camera.rotation);
-		var angle = camera.rotation.y;
-		var angleDegres = todegrees(angle);
-		var pointX = centerOfItem.x;
-		var pointZ = centerOfItem.z;
+		var newRotation = this.angleToOrigine - camera.rotation;
+			if(newRotation <0) newRotation  += k360degres;
+			if(newRotation >k360degres ) newRotation  -= k360degres;
 		
-		var originX = camera.position.x;
-		var originZ = camera.position.z;	
+		var sin = Math.sin(newRotation);
+		var cos = Math.cos(newRotation);
+		var newPositionFromCenter = {
+			"x": Math.floor(sin*this.distance),
+			"y": this.altitude,
+			"z":-Math.floor(cos*this.distance)
+		};
+		var newDistance = this.distance; // for the present we dont walk
+		// var diffX = newPositionFromCenter.x - camera.position.x;
+		// var diffY = newPositionFromCenter.y - camera.position.y;
+		// var diffZ = newPositionFromCenter.z - camera.position.z;
+
+		// var newDistance = hypo(diffX,diffY,diffZ);
+		// var newPosition = {
+			// "x": Math.floor(sin*this.distance),
+			// "y": this.altitude,
+			// "z":-Math.floor(cos*this.distance)
+		// };
 		
-		var cos = Math.cos(angle);
-		var sin = Math.sin(angle) ;
-		
-		var dX = pointX - originX;
-		var dZ = pointZ - originZ;
-		
-		var x =  cos * dX - sin * dZ + originZ;
-		var z =  sin * dX + cos * dZ + originZ;
-		
-		var distance = Math.sqrt(x*x+z*z);
-		
-		centerOfItem.x = x;
-		centerOfItem.z = z;
-		
-		console.log("item : " + this.name +" " + Math.floor(centerOfItem.x)+"," + Math.floor(centerOfItem.z));
+doDraw = Math.abs(newRotation) < k90degres;
 		
 	var doDraw = true; // centerOfItem.z >0;
 		if(doDraw){
@@ -178,23 +195,25 @@ function Shape(geometry,shapeSize,shapePosition,shapeRotation,name){
 				var copyOfPoint = {"x":point.x,"y":point.y,"z":point.z};
 				points.push(copyOfPoint);
 			});
-			points = doRotate(points,this.rotation.x,this.rotation.y,this.rotation.z);//-camera.rotation.y,
+			points = doRotate(points,this.rotation.x,this.rotation.y,this.rotation.z);
 			var points2D = [];
-			var shapeSize =  this.shapeSize;
-			var shapePosition = this.shapePosition;
+			var size =  this.size;
+			var position = this.position;
+			//newPositionFromCenter
+			//newRotation
 			points.forEach(function(point){
-				point.x *= shapeSize;
-				point.y *= shapeSize;
-				point.z *= shapeSize;
+				point.x *= size;
+				point.y *= size;
+				point.z *= size;
 				
-				point.x += centerOfItem.x;
-				point.y += centerOfItem.y;
-				point.z += centerOfItem.z;
+				point.x += newPositionFromCenter.x;
+				point.y += newPositionFromCenter.y;
+				point.z += newPositionFromCenter.z;
 				
 								
-				scale=fov/(fov+distance);
-				var x = Math.floor(point.x*scale+w2);
-				var y = Math.floor(point.y*scale+h2);
+				scale=fov/(fov+newDistance);
+				var x = Math.floor(point.x*scale);
+				var y = Math.floor(point.y*scale);
 				points2D.push({"x":x,"y":y});
 			});
 			context.beginPath();
@@ -269,12 +288,12 @@ function doRotate(points,pitch, roll, yaw) {
 				camera.turn(1);
 				break;
 			case 38: // up
-			    camera.walk(1);
-				needUpdate = true;
+			    //camera.walk(1);
+				//needUpdate = true;
 				break;
 			case 40: // down
-				camera.walk(-1);
-				needUpdate = true;
+				//camera.walk(-1);
+				//needUpdate = true;
 				break;
 		}
 	}); 
@@ -284,7 +303,7 @@ function doRotate(points,pitch, roll, yaw) {
 		height = canvas.height = window.innerHeight
 		w2 = width/2;
 		h2 = height/2;
-		fov = width /2;
+		fov = width;
 		needUpdate = true;
 	};
 }
